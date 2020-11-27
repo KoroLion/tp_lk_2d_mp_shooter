5d82bc9b4abb4a06cf5b65357d2e0f4d1da03354
Sergey Tyapkin <Tyapkin2002@mail.ru>
2020-11-28 02:33:01 +0300

    Fix trassers in SEEK_ROTATION mode

--- src/client/Animations.cpp
+++ src/client/Animations.cpp
@@ -9,15 +9,23 @@ Copyright 2020 LioKor Team (KoroLion, SergTyapkin, altanab)
 #include "SDL.h"
 
 void LightTrasser::render(SDL_Renderer* renderer, float baseX, float baseY, float centerRotation, float centerX, float centerY, float angle) {
+    if (texture == NULL)
+        return;
+
     float radAngle = -centerRotation/180*M_PI;
     float difX = x - centerX;
     float difY = y - centerY;
     float resX = difX*cos(radAngle) - difY*sin(radAngle);
     float resY = difX*sin(radAngle) + difY*cos(radAngle);
-    centerX -= baseX;
-    centerY -= baseY;
-    resX += centerX;
-    resY += centerY;
+    resX += centerX - baseX;
+    resY += centerY - baseY;
+
+    float difXEnd = xEnd - centerX;
+    float difYEnd = yEnd - centerY;
+    float resXEnd = difXEnd*cos(radAngle) - difYEnd*sin(radAngle);
+    float resYEnd = difXEnd*sin(radAngle) + difYEnd*cos(radAngle);
+    resXEnd += centerX - baseX;
+    resYEnd += centerY - baseY;
 
     centerRotation = rotation - centerRotation; // now it's absolute rotation
 
@@ -27,16 +35,15 @@ void LightTrasser::render(SDL_Renderer* renderer, float baseX, float baseY, floa
     int widthOfOne = (float)height * texture_w/texture_h;
     float wDif = texture_w / patterns;
     float clipX = 0;
-    float width = distance(x,y, xEnd,yEnd);
+    float width = distance(resX,resY, resXEnd,resYEnd);
     float global_patterns = width / widthOfOne * patterns;
-    float xDif = (xEnd - x) / global_patterns;
-    float yDif = (yEnd - y) / global_patterns;
+    float xDif = (resXEnd - resX) / global_patterns;
+    float yDif = (resYEnd - resY) / global_patterns;
     width /= global_patterns;
     float dist;
     for (int i = 0; i < global_patterns; i++) {
-        if ((dist = distance(x + xDif*i,y + yDif*i, xEnd,yEnd)) < 300) {
-            SDL_SetTextureAlphaMod(texture, 0xFF * (300-dist)/300);
-
+        if ((dist = distance(resX, resY, resXEnd,resYEnd)) < 300) {
+            SDL_SetTextureAlphaMod(texture, 0xFF * (1 - dist/300));
             SDL_Rect clip_rect {(int)(clipX), 0, (int)(clipX + wDif), (int)(texture_h)};
             SDL_Rect render_rect {(int)(resX), (int)(resY), (int)(width), (int)(height)};
             SDL_Point rotatePoint {0, 0};
