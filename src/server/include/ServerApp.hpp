1f5b2de843cc7e27400380a107a28e9b349f0c3c
KoroLion <korolion31@yandex.ru>
2020-12-10 17:31:41 +0300

    Mock world and data serialization

--- src/server/include/ServerApp.hpp
+++ src/server/include/ServerApp.hpp
@@ -5,11 +5,31 @@
 
 #include <vector>
 #include <string>
+#include <sstream>
 #include <thread>
 
 #include "utils.hpp"
 #include "World.hpp"
 #include "TcpServer.hpp"
+
+
+std::string game_objects_to_json(const std::vector<GameObject>& game_objects) {
+    std::stringstream ss;
+    ss << "[";
+    for (auto itr = game_objects.begin(); itr != game_objects.end(); itr++) {
+        ss << "{";
+        ss << "\"typeId\": " << itr->get_type_id() << ", ";
+        ss << "\"x\": " << itr->get_x() << ", ";
+        ss << "\"y\": " << itr->get_y();
+        ss << "}";
+        if (itr != (game_objects.end() - 1)) {
+            ss << ", ";
+        }
+    }
+    ss << "]";
+
+    return ss.str();
+}
 
 class ServerApp {
  private:
@@ -36,15 +56,18 @@ class ServerApp {
     void net_notify() {
         while (!net_server->is_running()) {}  // waiting for server to start
         while (net_server->is_running()) {
-            // std::string data = to_json(world->get_objects());
-            std::string data = "WOLF";
+            const std::vector<GameObject>& game_objects = world->get_game_objects(1);
+
+            std::string data = game_objects_to_json(game_objects);
+
+            std::cout << data << std::endl;
             net_server->send_all(data);
             sleep_ms(1000);
         }
     }
 
     void game_world() {
-        // world->start();
+        world->start();
     }
 
     void handle_cmd() {
@@ -54,10 +77,12 @@ class ServerApp {
             if (command == "stop") {
                 std::cout << "Stopping network..." << std::endl;
                 net_server->stop();
-                // world->stop();
+                world->stop();
 
                 std::cout << "Stopping server..." << std::endl;
                 running = false;
+            } else {
+                std::cout << "Unknown command!" << std::endl;
             }
         }
     }
